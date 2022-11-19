e5c66dcbba4c0704cfd5cae1c9c5e11e59bdd796
Udoh Levi <ukaraobong15@gmail.com>
2022-11-19 12:56:20 +0100

    type error fixed

--- 190407076_Assignment/control_structure/Exercise_2.py
+++ 190407076_Assignment/control_structure/Exercise_2.py
@@ -1,7 +1,8 @@
 # Script continuously generates a random integer between 1 and 15 inclusively as long as the value generated is less than or equal to 10. 
 # Script also states if the number generated is an even or odd number i.e., if 1 is generated, it prints 1 is an odd number, if 6 is generated, it prints 6 is an even number
-from random import randint
-for num in randint(1, 15):
+from random import randrange
+for num in range(1, 15):
+    x = randrange(15)
     if num <= 10:
         if num % 2 == 0:
             print(f"{num} is an odd number")
