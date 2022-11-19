2f8eb0e67ef8a324ffda74c8c3e7f63dd97e8f93
Udoh Levi <ukaraobong15@gmail.com>
2022-11-19 13:02:04 +0100

    fixed rand int problem

--- 190407076_Assignment/control_structure/Exercise_2.py
+++ 190407076_Assignment/control_structure/Exercise_2.py
@@ -1,10 +1,10 @@
 # Script continuously generates a random integer between 1 and 15 inclusively as long as the value generated is less than or equal to 10. 
 # Script also states if the number generated is an even or odd number i.e., if 1 is generated, it prints 1 is an odd number, if 6 is generated, it prints 6 is an even number
-from random import randrange
+from random import randint
 for num in range(1, 15):
-    x = randrange(15)
-    if num <= 10:
-        if num % 2 == 0:
-            print(f"{num} is an odd number")
+    x = randint(1, 15)
+    if x <= 10:
+        if x % 2 == 0:
+            print(f"{x} is an odd number")
         else:
-            print(f"{num} is an even number")
+            print(f"{x} is an even number")
